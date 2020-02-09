91222f03f51a4bc2e19f05bf227b2c789d5d4f52
Scott W Harden <swharden@gmail.com>
2020-02-08 23:53:15 -0500

    Update Calculate.cs

--- src/LJPmath/Calculate.cs
+++ src/LJPmath/Calculate.cs
@@ -9,11 +9,20 @@ namespace LJPmath
     {
         public static Result NernstPlank(List<Ion> ionList)
         {
+
             Debug.WriteLine($"calculating LJP using the stationary Nernst-Planck equation for {ionList.Count} ions...");
 
             foreach (Ion ion in ionList)
                 Debug.WriteLine(ion);
 
+            // get starting phis for each ion
+            double[] phis = new double[ionList.Count - 2];
+            for (int j = 0; j < ionList.Count - 2; j++)
+                phis[j] = ionList[j].conc2_M - ionList[j].conc1_M;
+
+            // TODO: this is where I stopped working...
+
+            // done
             var result = new Result();
             return result;
         }
