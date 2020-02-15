251f71d69f4736f4798b5c10bdf436974e9375a6
Scott W Harden <swharden@gmail.com>
2020-02-15 18:18:59 -0500

    remove vacuum permittivity from constants

--- src/LJPmath/Constants.cs
+++ src/LJPmath/Constants.cs
@@ -6,12 +6,9 @@ namespace LJPmath
 {
     public static class Constants
     {
-		// TODO: swap these to use full precision
-
+		// Boltzmann constant = 1.38064852e-23 (m^2 * kg) / (s^2 * K)
 		public const double zeroCinK = 273.15;
 		public const double temperatureC = 25.0;
-
-		// Boltzmann constant = 1.38064852e-23 (m^2 * kg) / (s^2 * K)
 		public const double boltzmann = 1.38064852e-23;
 		public const double KT = boltzmann * (temperatureC + zeroCinK);
 
@@ -20,8 +17,5 @@ namespace LJPmath
 
 		// Avogadro constant = 6.02214076e23 (no units)
 		public const double Nav = 6.02214076e23;
-
-		// Vacuum permittivity = 8.8541878128e-12 (F / m)
-		//public const double epsilon = 8.8541878128e-12;
 	}
 }
