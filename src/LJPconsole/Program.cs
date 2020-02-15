7d7b83efce12fbe4e14bb78c1c4fcf88b048167d
Scott W Harden <swharden@gmail.com>
2020-02-15 14:50:16 -0500

    Update Program.cs

--- src/LJPconsole/Program.cs
+++ src/LJPconsole/Program.cs
@@ -9,25 +9,61 @@ namespace LJPconsole
     {
         static void Main(string[] args)
         {
-            RunLjpCalcTest();
-        }
-
-        private static void RunLjpCalcTest()
-        {
-            var ionTable = new IonTable();
-
             var ionSet = new List<Ion>
             {
-                new Ion(ionTable.Lookup("Zn"), 9, 0.0284),
-                new Ion(ionTable.Lookup("K"), 0, 3), // second from last is "X"
-                new Ion(ionTable.Lookup("Cl"), 18, 3.0568) // last becomes "last"
+                new Ion("Zn", 9, 0.0284),
+                new Ion("K", 0, 3),
+                new Ion("Cl", 18, 3.0568)
             };
 
-            double calculated_mV = Calculate.LjpForIons(ionSet) * 1000;
-            Console.WriteLine($"LJP: {calculated_mV} mV"); // -20.8238808914194
+            var ionTable = new IonTable();
+            ionSet = ionTable.Lookup(ionSet);
+            DisplayIonSet(ionSet);
 
-            Console.WriteLine("\npress ENTER to exit...");
-            Console.ReadLine();
+            double ljp = Calculate.LjpForIons(ionSet);
+            Console.WriteLine($"LJP = {ljp * 1000} mV");
+
+            Exit();
+        }
+
+        private static void Exit(bool pauseFirst = true)
+        {
+            if (pauseFirst)
+            {
+                Console.WriteLine("\npress ENTER to exit...");
+                Console.ReadLine();
+            }
+            Environment.Exit(0);
+        }
+
+        private static void DisplayIonSet(List<Ion> ionSet)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("ION TABLE:");
+
+            int columnWidth = 10;
+            Console.Write("Ion".PadRight(10));
+            Console.Write("Charge".PadRight(7));
+            Console.Write("Cond (E4)".PadRight(columnWidth));
+            Console.Write("Mu (E11)".PadRight(columnWidth));
+            Console.Write("Phi".PadRight(columnWidth));
+            Console.Write("c0".PadRight(columnWidth));
+            Console.Write("cL".PadRight(columnWidth));
+            Console.WriteLine("");
+
+            foreach (Ion ion in ionSet)
+            {
+                Console.Write(ion.name.PadRight(10));
+                Console.Write(ion.charge.ToString().PadRight(7));
+                Console.Write((ion.conductance * 1e4).ToString().PadRight(columnWidth));
+                Console.Write(Math.Round(ion.mu / 1E11, 5).ToString().PadRight(columnWidth));
+                Console.Write(ion.phi.ToString().PadRight(columnWidth));
+                Console.Write(ion.cL.ToString().PadRight(columnWidth));
+                Console.Write(ion.c0.ToString().PadRight(columnWidth));
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("");
         }
     }
 }
