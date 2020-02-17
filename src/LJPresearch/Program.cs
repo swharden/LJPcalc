e727970f338fd1b0547e7fef9b65647135ecb07a
Scott W Harden <swharden@gmail.com>
2020-02-17 08:11:45 -0500

    create research module

---
+++ src/LJPresearch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LJPresearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoScottPlot();
+        }
+
+        public static void DemoScottPlot()
+        {
+            double[] xs = ScottPlot.DataGen.Consecutive(101);
+            double[] ys1 = ScottPlot.DataGen.Sin(xs.Length, 2);
+            double[] ys2 = ScottPlot.DataGen.Cos(xs.Length, 2);
+
+            var plt = new ScottPlot.Plot(600, 400);
+            plt.PlotScatter(xs, ys1, label: "Nernst-Planck");
+            plt.PlotScatter(xs, ys2, label: "Henderson");
+            plt.YLabel("LJP (mV)");
+            plt.XLabel("Alpha (%)");
+            plt.Title("Experimental Plot Test");
+            plt.AxisAuto(0, .3);
+            plt.Legend();
+
+            string filePath = System.IO.Path.GetFullPath("Figure_ScottPlotDemo.png");
+            plt.SaveFig(filePath);
+            Console.WriteLine($"Saved: {filePath}");
+        }
+    }
+}
