ef5c998cf7e948c8f6337cf59f19ff2debd0ae9c
Scott W Harden <swharden@gmail.com>
2020-03-30 18:59:46 -0400

    test that failing ion sets pass when sorted

--- src/LJPmath/KnownIonSets.cs
+++ src/LJPmath/KnownIonSets.cs
@@ -111,7 +111,7 @@ namespace LJPmath
 
             /*
             ionSets.Add(new KnownIonSet(
-                name: "CRASH #9",
+                name: "CRASH #9A",
                 details: "Known to crash due to poor ion order https://github.com/swharden/LJPcalc/issues/9",
                 expectedLjp_mV: 0,
                 temperatureC: 25,
@@ -122,6 +122,23 @@ namespace LJPmath
                         new Ion("Cl", 18, 3.062),
                         new Ion("Mg", 5, 3),
                         new Ion("Ag", 1, 1),
+                },
+                ionTable: ionTable));
+
+            ionSets.Add(new KnownIonSet(
+                name: "CRASH #9B",
+                details: "Known to crash due to poor ion order https://github.com/swharden/LJPcalc/issues/9",
+                expectedLjp_mV: 0,
+                temperatureC: 25,
+                expectedAccuracy_mV: 9999,
+                ions: new List<Ion>(){
+                        new Ion("K", 145, 2.8),
+                        new Ion("Na", 13, 145),
+                        new Ion("Cl", 10, 148.8),
+                        new Ion("Gluconate", 145, 0),
+                        new Ion("Mg", 1, 2),
+                        new Ion("Ca", 0, 1),
+                        new Ion("HEPES", 5, 5),
                 },
                 ionTable: ionTable));
                 */
