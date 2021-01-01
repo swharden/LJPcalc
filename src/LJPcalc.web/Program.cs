d90fcee1e427cd6c39f2fdd51d501c4d83b0f8bd
Scott W Harden <swharden@gmail.com>
2021-01-01 15:39:24 -0500

    create singleton LjpService

--- src/LJPcalc.web/Program.cs
+++ src/LJPcalc.web/Program.cs
@@ -19,6 +19,8 @@ namespace LJPcalc.web
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddSingleton<Services.LjpService>();
+
             await builder.Build().RunAsync();
         }
     }
---
+++ src/LJPcalc.web/Services/LjpService.cs
@@ -0,0 +1,35 @@
+﻿using LJPmath;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LJPcalc.web.Services
+{
+    public class LjpService
+    {
+        private readonly List<Ion> IonList = new List<Ion>();
+
+        public string ResultText { get; private set; }
+        public double ResultLJP { get; private set; }
+
+        public string Version =>
+            typeof(Ion).Assembly.GetName().Version.Major + "." +
+            typeof(Ion).Assembly.GetName().Version.Minor;
+
+        public void AddIon(string name, int charge, double conductivity, double concA, double concB) =>
+            IonList.Add(new Ion(name, charge, conductivity, concA, concB));
+
+        public void Clear() => IonList.Clear();
+
+        public void RemoveAt(int index) => IonList.RemoveAt(index);
+
+        public void CalculateLJP(double temperatureC)
+        {
+            var result = Calculate.Ljp(IonList, temperatureC);
+            ResultText = result.report;
+            ResultLJP = result.mV;
+        }
+    }
+}
