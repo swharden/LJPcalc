79645bf24974a2a10fb9c7619171940a2974e459
Scott W Harden <swharden@gmail.com>
2024-02-08 10:31:53 -0500

    delete old dev files

--- dev/build-site.bat
+++
@@ -1,4 +0,0 @@
-rmdir /s /q ..\src\LJPcalc.Web\bin\
-dotnet publish --configuration Release ..\src\LJPcalc.Web\
-explorer ..\src\LJPcalc.Web\bin\Release\net6.0\publish\wwwroot
-pause
--- dev/scripts/Bundle/LJPconsole.bat
+++
@@ -1,7 +0,0 @@
-:: This batch file makes it easy to run LJPconsole on Windows
-::
-:: Place this file in the same folder as LJPconsole.dll
-
-dotnet LJPconsole.dll
-
-pause
